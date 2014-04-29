947f8aaa024d1dd7a634f6b9c3fbea26917395c1
Rodrigo Habib Gregori <rodrigo.gregori@gmail.com>
2014-04-29 19:53:18 -0300

    Initial Commit

---
+++ stlSlicer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  stlSlicer
+//
+//  Created by Rodrigo Habib Gregori on 29/04/14.
+//  Copyright (c) 2014 Rodrigo Habib Gregori. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
